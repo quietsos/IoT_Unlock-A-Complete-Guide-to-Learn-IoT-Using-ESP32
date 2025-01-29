6062af3e15611d815d98e3232c8287d8c883aab3
quietsos <shohaniu8@gmail.com>
2025-01-29 22:23:30 +0600

    LM35 Sensor Code for Arduino plantform

---
+++ LM35_temperature_sensor/LM35_temperature_sensor.ino
@@ -0,0 +1,28 @@
+/*
+ Md Shohanuzzaman
+ IoT Engineer @ IoT Lab
+ */
+
+#define ADC_VREF_mV 3300.0
+#define ADC_RESOLUTION 4096.0
+#define PIN_LM35 14
+ 
+void setup() {
+  Serial.begin(9600);
+}
+ 
+void loop() {
+  int adcVal = analogRead(PIN_LM35);
+  float milliVolt = adcVal * (ADC_VREF_mV / ADC_RESOLUTION);
+  float tempC = milliVolt / 10;
+  float tempF = tempC * 9 / 5 + 32;
+ 
+  Serial.print("Temperature: ");
+  Serial.print(tempC);
+  Serial.print("°C");
+  Serial.print(".....");
+  Serial.print(tempF);
+  Serial.println("°F");
+ 
+  delay(1000);
+}
